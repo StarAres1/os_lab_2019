60c42130998307b15293f9c7d297355188a6410c
StormLight <124205878+StarAres1@users.noreply.github.com>
2024-11-03 12:35:27 +0000

    Первое задание сделано

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -9,7 +9,7 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  for(int i = begin - 1; i < end; i++){
+  for(int i = begin; i < end; i++){
     if (array[i] < min_max.min){
       min_max.min = array[i];
     }
@@ -21,34 +21,4 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   
   return min_max;
 }
-
-  int main(int argc, char *argv[]){
-    if (argc < 4){
-      printf("Недостаточное количество аргументов");      
-      return 1;
-    }  
-
-    int size = argc - 3; // первый аргумент имя программы, следующие два наши индексы
-    int array[size];
-
-
-    for (int i = 0; i < size; i++) {
-      array[i] = atoi(argv[i + 3]); // Преобразование строковых аргументов в целые числа
-    }
-
-    unsigned int begin = atoi(argv[1]);
-    unsigned int end = atoi(argv[2]);
-
-    if (begin > end || end > size){
-      printf("Ошибка при вводе индексов диапазона");
-      return 1;
-    }
-
-    struct MinMax result = GetMinMax(array, begin, end);
-    printf("Минимум: %d\n", result.min);
-    printf("Максимум: %d\n", result.max);
-
-    return 0;
-
-  }
 
--- lab3/src/readme.md
+++ lab3/src/readme.md
@@ -1,8 +1,26 @@
-# Задание 1
+# Задание 1  
+
 ---
-![alt text](image.png)
----
-Проверим что есть подхожящий компилятор
+![alt text](image.png)  
+---  
+
+```bash
+@StarAres1 ➜ /workspaces/os_lab_2019 (master) $ gcc --version
+
+@StarAres1 ➜ /workspaces/os_lab_2019 (master) $ cd lab3
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3 (master) $ cd src
+
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3/src (master) $ gcc -c find_min_max.c
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3/src (master) $ gcc -c utils.c
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3/src (master) $ gcc -c sequantial_min_max.c
+
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3/src (master) $ gcc find_min_max.o utils.c sequential_min_max.o -o min_max
+@StarAres1 ➜ /workspaces/os_lab_2019/lab3/src (master) $ ./min_max 1 3
+```
 ![alt text](image-1.png)
-Перейдем в папку с файлом, скомпилуем его, а после запустим с входными аргументами
+# Задание 2-3   
+
+---
 ![alt text](image-2.png)
+---
+
